8af55f93a2422ee3a596c787fa9e9ed01dd8df54
Gilles Van De Vyver <gilles.van.de.vyver@ntnu.no>
2023-11-07 09:55:50 +0100

    Add default CONST.py

---
+++ CONST.py
@@ -0,0 +1,11 @@
+# location of preprocessed data
+
+# Preprocessed data in 'two ring' format
+CAMUS = "data/local_data/CAMUS_keypoint_annotations/"
+
+# Preprocessed data in 'displacement' format
+CAMUS_displacament = "data/local_data/CAMUS_keypoint_annotations_displacement/"
+
+
+# location of your experiments
+STORAGE_DIR = "./experiments/"
--- CONST_EXAMPLE.py
+++
@@ -1,8 +0,0 @@
-# location of your data
-US_MultiviewData_MASTER = "/shared-data5/guy/data/EchoNet-Dynamic/"
-
-# location of your data
-US_MultiviewData = "/mntssd/guy/data/EchoNet-Dynamic/"
-
-# location of your experiments
-STORAGE_DIR = "/shared-data5/guy/exps/echonet/storage/"
